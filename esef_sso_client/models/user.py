030bd501722f234f1c5a21ff8761fb935380767c
Ahmet DAL <ceahmetdal@gmail.com>
2014-09-25 18:38:13 +0300

    minor changes.

--- esef_sso_client/models/user.py
+++ esef_sso_client/models/user.py
@@ -6,9 +6,9 @@ from django.utils.translation import ugettext_lazy as _
 __author__ = 'ahmetdal'
 
 
-class EsefUser(get_user_model()):
+class EsefSSOUser(get_user_model()):
     class Meta:
-        app_label = 'esef'
+        app_label = 'esef_sso'
 
     access_token = models.CharField(_('Last Access Token'), max_length=200, null=True, blank=True)
 
